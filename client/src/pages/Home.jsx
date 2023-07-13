ffca76b4d27c2e3d33d46cc8d124ecdd9908f4f9
hardik3018 <hardik.mca21.du@gmail.com>
2023-07-13 16:17:57 +0530

    updated home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import  Banner  from "./banner";
+import Display from "./display";
 
 export const Home = () => {
     return(
-       <>
+       <section id="home">
        <Banner/>
-       </>
+       <Display/>
+       </section>
     );
 }
 
---
+++ client/src/pages/display.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "../styles/display.css"
+
+function Display() {
+  return (
+    <section id='display'>
+        <div className='display'>
+            <div className='d1'>
+
+            </div>
+            
+            <div className='d2'>
+
+                <div className='d2-box'>
+
+                </div>
+                <div className='d2-box btm'>
+
+                </div>
+
+            </div>
+
+            <div className='d3'>
+
+            </div>
+        </div>
+
+            <div className='d4'>
+
+            </div>
+
+            <div className='d5'>
+                
+            </div>
+        
+
+    </section>
+  )
+}
+
+export default Display
